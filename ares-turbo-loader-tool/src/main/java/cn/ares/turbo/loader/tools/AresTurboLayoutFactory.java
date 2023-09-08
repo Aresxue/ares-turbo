2c0c486f40c6077918d59c1cf3284f18504b0677
zuoyou.xb <ares@come-future.com>
2023-09-08 16:43:30 +0800

    fix: implements getLayout

--- ares-turbo-loader-tool/src/main/java/cn/ares/turbo/loader/tools/AresTurboLayoutFactory.java
+++ ares-turbo-loader-tool/src/main/java/cn/ares/turbo/loader/tools/AresTurboLayoutFactory.java
@@ -1,13 +1,16 @@
 package cn.ares.turbo.loader.tools;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.Locale;
 import org.springframework.boot.loader.tools.CustomLoaderLayout;
-import org.springframework.boot.loader.tools.DefaultLayoutFactory;
+import org.springframework.boot.loader.tools.Layout;
+import org.springframework.boot.loader.tools.LayoutFactory;
 import org.springframework.boot.loader.tools.Layouts;
 import org.springframework.boot.loader.tools.LoaderClassesWriter;
 
 
-public class AresTurboLayoutFactory extends DefaultLayoutFactory {
+public class AresTurboLayoutFactory implements LayoutFactory {
 
   private static final String NESTED_LOADER_JAR = "META-INF/loader/spring-boot-loader.jar";
 
@@ -30,6 +33,25 @@ public class AresTurboLayoutFactory extends DefaultLayoutFactory {
       // 替换为自定义的JarLauncher
       // Replace with a custom JarLauncher
       return "cn.ares.turbo.loader.AresJarLauncher";
+    }
+  }
+
+  public Layout getLayout(File file) {
+    if (file == null) {
+      throw new IllegalArgumentException("File must not be null");
+    } else {
+      String lowerCaseFileName = file.getName().toLowerCase(Locale.ENGLISH);
+      if (lowerCaseFileName.endsWith(".jar")) {
+        // 返回自定义的jar
+        // return custom jar
+        return new Jar();
+      } else if (lowerCaseFileName.endsWith(".war")) {
+        return new Layouts.War();
+      } else if (!file.isDirectory() && !lowerCaseFileName.endsWith(".zip")) {
+        throw new IllegalStateException("Unable to deduce layout for '" + file + "'");
+      } else {
+        return new Layouts.Expanded();
+      }
     }
   }
 
